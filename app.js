f24055be6a80607f04d732e7b56d155f883e529f
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2024-07-03 22:54:20 -0600

    Initial transcription functionality

---
+++ app.js
@@ -0,0 +1,76 @@
+async function init() {
+  try {
+    const websocket = createWebSockets();
+
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    const videoElement = document.getElementById("video");
+    videoElement.srcObject = stream;
+
+    let audioTrack = stream.getAudioTracks()[0];
+    console.log(audioTrack);
+    const audioOnlyStream = new MediaStream();
+    audioOnlyStream.addTrack(audioTrack);
+
+    createMediaRecorder(audioOnlyStream, websocket);
+
+    const button = document.getElementById("stopPlayback");
+    button.onclick = (e) => {
+      websocket.close();
+
+      let videoTrack = stream.getVideoTracks()[0];
+      let audioTrack = stream.getAudioTracks()[0];
+      stream.removeTrack(videoTrack);
+      stream.removeTrack(audioTrack);
+
+      let audioOnlyAudioTrack = audioOnlyStream.getAudioTracks()[0];
+      audioOnlyStream.removeTrack(audioOnlyAudioTrack);
+    };
+  }
+  catch (error) {
+    console.log("Initialization error: ", error);
+  }
+}
+
+function createWebSockets() {
+  // const wsUri = "ws://35.184.118.175";
+  const wsUri = "ws://127.0.0.1";
+  const websocket = new WebSocket(wsUri);
+
+  websocket.onopen = (e) => {
+    console.log("Connected");
+  };
+
+  websocket.onclose = (e) => {
+    console.log("Disconnected");
+  };
+
+  websocket.onmessage = (e) => {
+    console.log("Message received:", e.data);
+  };
+
+  websocket.onerror = (e) => {
+    console.log("WebSocket error: ", e.data);
+  };
+  return websocket;
+}
+
+function createMediaRecorder(stream, websocket) {
+  const recorder = new MediaRecorder(stream);
+  recorder.start(1000);
+  console.log("Media recorder state: ", recorder.state);
+  recorder.ondataavailable = (e) => {
+    if (e.data.size > 0) {
+      console.log("Data sent: ", e.data);
+      websocket.send(e.data);
+    }
+  };
+  recorder.onstop = (e) => {
+    console.log("Recorder has stopped");
+  };
+  recorder.onerror = (e) => {
+    console.log("Recorder error: ", e.data);
+  };
+}
+
+init()
+
---
+++ main.js
@@ -0,0 +1,56 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 80 });
+const speech = require('@google-cloud/speech');
+const client = new speech.SpeechClient();
+
+const request = {
+  config: {
+    encoding: 'WEBM_OPUS',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US',
+  }
+};
+
+wss.on('connection', function connection(ws) {
+  console.log('WebSocket connected');
+  let activeTimer = false;
+  let buffer = "";
+  let timer = null;
+  const recognizeStream = client
+    .streamingRecognize(request)
+    .on('error', console.error)
+    .on('data', data => {
+      if (data.results[0] && data.results[0].alternatives[0] && (data.results[0].alternatives[0].transcript != "")) {
+        result = data.results[0].alternatives[0].transcript;
+        // buffer = buffer + result + " ";
+        buffer = buffer + result;
+        if (!activeTimer) {
+          activeTimer = true;
+          timer = setTimeout(() => {
+            console.log(buffer);
+            buffer = "";
+            activeTimer = false;
+          }, 3000);
+        }
+        else if (activeTimer) {
+          clearTimeout(timer);
+          timer = setTimeout(() => {
+            console.log(buffer);
+            buffer = "";
+            activeTimer = false;
+          }, 3000);
+        }
+      }
+      else {
+        console.log("Unidentified output");
+      }
+    });
+
+  ws.on('message', function incoming(data) {
+    recognizeStream.write(data);
+  });
+
+  ws.onclose = (e) => {
+    console.log("Disconnected");
+  };
+});
