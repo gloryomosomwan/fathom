b914513388100fd1bf4a6a6ab4670d73cbdb4d28
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2024-07-18 12:43:30 -0600

    Commented out buffer logic that causes the speech delay

--- server/main.js
+++ server/main.js
@@ -76,38 +76,44 @@ function startRecognizeStream(socket, request, lang) {
     console.log('Invalid lang');
   }
 
-  let activeTimer = false;
-  let buffer = "";
-  let timer = null;
+  // let activeTimer = false;
+  // let buffer = "";
+  // let timer = null;
   console.log('streaming started...');
   let recognizeStream = client
     .streamingRecognize(request)
     .on('error', console.error)
     .on('close', () => console.log('Closed recognizeStream'))
-    .on('data', data => {
+    .on('data', async (data) => {
       if (data.results[0] && data.results[0].alternatives[0] && (data.results[0].alternatives[0].transcript != "")) {
         result = data.results[0].alternatives[0].transcript;
-        buffer = buffer + result;
-        if (!activeTimer) {
-          activeTimer = true;
-          timer = setTimeout(async () => {
-            let translation = await translate(buffer, target);
-            console.log(translation);
-            let speech = await synthesize(translation, languageCode);
-            const speechBlob = Buffer.from(speech, 'binary');
-            socket.send(speechBlob);
-            buffer = "";
-            activeTimer = false;
-          }, 3000);
-        }
-        else if (activeTimer) {
-          clearTimeout(timer);
-          timer = setTimeout(() => {
-            console.log(buffer);
-            buffer = "";
-            activeTimer = false;
-          }, 3000);
-        }
+        console.log('Transcription:', result);
+        // buffer = buffer + result;
+        // if (!activeTimer) {
+        //   activeTimer = true;
+        //   timer = setTimeout(async () => {
+        //     let translation = await translate(buffer, target);
+        //     console.log(translation);
+        //     let speech = await synthesize(translation, languageCode);
+        //     const speechBlob = Buffer.from(speech, 'binary');
+        //     socket.send(speechBlob);
+        //     buffer = "";
+        //     activeTimer = false;
+        //   }, 3000);
+        // }
+        // else if (activeTimer) {
+        //   clearTimeout(timer);
+        //   timer = setTimeout(() => {
+        //     console.log(buffer);
+        //     buffer = "";
+        //     activeTimer = false;
+        //   }, 3000);
+        // }
+        let translation = await translate(result, target);
+        console.log('Translation:', translation);
+        let speech = await synthesize(translation, languageCode);
+        const speechBlob = Buffer.from(speech, 'binary');
+        socket.send(speechBlob);
       }
       else {
         console.log("Unidentified output");
